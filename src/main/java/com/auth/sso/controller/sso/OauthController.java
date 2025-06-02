0a079cba963d4ff49145479f69078ec29c2d9fd1
yangte <1473453000@qq.com>
2025-06-02 22:41:45 +0800

    无用代码删除

--- src/main/java/com/auth/sso/controller/sso/OauthController.java
+++ src/main/java/com/auth/sso/controller/sso/OauthController.java
@@ -1,4 +1,4 @@
-package com.auth.sso.controller;
+package com.auth.sso.controller.sso;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
