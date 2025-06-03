eeeba6f072d6912b11b2657a674a6b128ec4d842
yangte <1473453000@qq.com>
2025-06-03 20:56:53 +0800

    无用代码删除

--- src/main/java/com/auth/sso/service/DbFieldInfoService.java
+++
@@ -1,7 +0,0 @@
-package com.auth.sso.service;
-
-import com.auth.sso.entity.DbFieldInfo;
-import com.baomidou.mybatisplus.extension.service.IService;
-
-public interface DbFieldInfoService extends IService<DbFieldInfo> {
-} 
--- src/main/java/com/auth/sso/service/DbTableInfoService.java
+++
@@ -1,7 +0,0 @@
-package com.auth.sso.service;
-
-import com.auth.sso.entity.DbTableInfo;
-import com.baomidou.mybatisplus.extension.service.IService;
-
-public interface DbTableInfoService extends IService<DbTableInfo> {
-} 
